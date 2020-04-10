0bd80f7311b8a3b9a926410372f1af63dd88218d
DavvM <davide2.mantegazza@mail.polimi.it>
2020-04-10 17:00:29 +0200

    Defined DivinityCard interface

--- ProvaFinaleIngSw2020/src/main/java/it/polimi/ingsw/PSP38/controller/DivinityCard.java
+++ ProvaFinaleIngSw2020/src/main/java/it/polimi/ingsw/PSP38/controller/DivinityCard.java
@@ -1,5 +1,35 @@
 package it.polimi.ingsw.PSP38.controller;
 
-public interface DivinityCard {
+import it.polimi.ingsw.PSP38.model.*;
 
+import java.util.List;
+import java.util.Map;
+
+public interface DivinityCard {
+    default List<Cell> preMove(Worker w, Board b){
+        List<Cell> neighborCells=b.neighborsCells(w.getPosition());
+        Map<Cell, Worker> workersPositions=b.getWorkersPositions();
+        neighborCells.removeIf(c -> workersPositions.containsKey(c) || c.hasDome() || c.getTowerHeight() > w.getPosition().getTowerHeight() + 1);
+        //Removes the current cell from the neighborCells list if the cell is already occupied or there is a complete tower or a tower whose level is
+        //more than player+1
+        return neighborCells;
+    }
+    default Board move(Worker w, Cell destinationCell, Board b){
+        Cell initialCell=w.getPosition();
+        Board newBoard=b.withWorker(w, destinationCell);
+        return newBoard;
+    }
+    default List<Cell> preBuild(Worker w, Board b){
+        List<Cell> neighborCells=b.neighborsCells(w.getPosition());
+        Map<Cell, Worker> workersPositions=b.getWorkersPositions();
+        neighborCells.removeIf(c -> workersPositions.containsKey(c) || c.hasDome());
+        //Removes the current cell from the neighborCells list if the cell is already occupied or there is a complete tower or a tower whose level is
+        //more than player+1
+        return neighborCells;
+    }
+    default Board build(Cell buildingCell, Board b){
+        Cell newCell = buildingCell.getTowerHeight() == 3 ? buildingCell.withDome() : buildingCell.withTowerHeight(buildingCell.getTowerHeight() + 1);
+        Board newBoard=b.withCell(newCell);
+        return newBoard;
+    }
 }
