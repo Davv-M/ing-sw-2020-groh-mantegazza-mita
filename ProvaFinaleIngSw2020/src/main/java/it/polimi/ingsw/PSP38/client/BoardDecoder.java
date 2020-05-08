f71b1c861fe90bb83de9dfa82a95d75c08f0c1f7
DavvM <davide2.mantegazza@mail.polimi.it>
2020-05-08 23:56:53 +0200

    Added JavaDoc to Client, BoardDecoder, BytesForBoard, Protocol classes

--- ProvaFinaleIngSw2020/src/main/java/it/polimi/ingsw/PSP38/client/BoardDecoder.java
+++ ProvaFinaleIngSw2020/src/main/java/it/polimi/ingsw/PSP38/client/BoardDecoder.java
@@ -3,22 +3,47 @@ package it.polimi.ingsw.PSP38.client;
 import it.polimi.ingsw.PSP38.common.BytesForBoard;
 import it.polimi.ingsw.PSP38.common.WorkerColor;
 
+/**
+ * This class includes methods used to decode the status of the cells of the board. For details regarding the encoding
+ * of the board's status, refer to the documentation of <code>BoardEncoder</code> class
+ */
 public class BoardDecoder {
 
+    /**
+     * Method used to check if it's possible to move a worker or build something onto a given cell of the board
+     * @param b represents the encoded cell that will be checked
+     * @return true if the cell is not occupied by a worker and no dome has been built on it, false otherwise
+     */
     public static boolean isFree(byte b) {
         return !(hasWorker(b) || hasDome(b));
     }
 
+    /**
+     * Method used to check if the given cell has a dome built on it
+     * @param b represents the encoded cell that will be checked
+     * @return true if the cell has a dome on it, false otherwise
+     */
     public static boolean hasDome(byte b) {
         return isAndEqual(b, BytesForBoard.DOME);
     }
 
+    /**
+     * Method used to check if the given cell is occupied by a worker
+     * @param b represents the encoded cell that will be checked
+     * @return true if a worker of any color is stationing on it, false otherwise
+     */
     public static boolean hasWorker(byte b) {
         return isAndEqual(b, BytesForBoard.WORKER_WHITE)
                 || isAndEqual(b, BytesForBoard.WORKER_BLACK)
                 || isAndEqual(b, BytesForBoard.WORKER_BLUE);
     }
 
+    /**
+     * Method used to determine the color of a worker
+     * @param b represents a generic encoded worker
+     * @return the color of the encoded worker
+     * @throws IllegalArgumentException if the code doesn't correspond to one of the defined colors
+     */
     public static WorkerColor workerColor(byte b) {
         if (isAndEqual(b, BytesForBoard.WORKER_WHITE)) {
             return WorkerColor.WHITE;
@@ -31,6 +56,11 @@ public class BoardDecoder {
         }
     }
 
+    /**
+     * Method used to determine the height of a given tower
+     * @param b represents a generic encoded tower
+     * @return the height of the tower represented by <code>b</code>
+     */
     public static int towerHeight(byte b) {
         if(isAndEqual(b, BytesForBoard.TOWER_1)){
             return 1;
@@ -43,6 +73,12 @@ public class BoardDecoder {
         }
     }
 
+    /**
+     * Auxiliary method used to check if two byte values are identical
+     * @param byteToCheck represents the first parameter to be checked
+     * @param flag represents the second parameter to be checked
+     * @return the result of the boolean operation <code>(byteToCheck & flag) == flag</code>
+     */
     private static boolean isAndEqual(byte byteToCheck, byte flag){
         return (byteToCheck & flag) == flag;
     }
